4127daa2d94232cc1052b64aa27292464cb39faf
BigBlackWolf <stepan.rasputny@gmail.com>
2017-03-17 13:40:10 +0200

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -11,3 +11,29 @@ int main () {
    first->Anakonda();
    return 0;
 }
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	return 0;
+};
+
+int vidnimanie (int a, int b) {
+	if ((a>b) && (a>=0) && (b>=0)) {
+     return (a-b); }	
+    else {
+     cout <<"ERROR";};
+};
+
+float vidnimanie (float a, float b) {
+	return (a-b);
+}
+
+int operator -(int a, int b) {
+	return (a-b);
+}
+
+float operator -(float a, float b) {
+	return (a+b)
+}
